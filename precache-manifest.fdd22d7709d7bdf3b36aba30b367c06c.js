98649e323f6430f720832e1f306770f6c238d1b4
Osaulenko Oleksandr <oom2212@gmail.com>
2020-06-12 20:23:13 +0300

    Updates

---
+++ precache-manifest.fdd22d7709d7bdf3b36aba30b367c06c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "742772f19132bccb94147abbfc133ace",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "14b3086a3668083132fc",
+    "url": "/goit-react-hw-02-feedback/static/css/main.d9917373.chunk.css"
+  },
+  {
+    "revision": "11c755667480b38c11de",
+    "url": "/goit-react-hw-02-feedback/static/js/2.0c7a6467.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.0c7a6467.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14b3086a3668083132fc",
+    "url": "/goit-react-hw-02-feedback/static/js/main.945d8801.chunk.js"
+  },
+  {
+    "revision": "67fc603d4fe28264fbd4",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.56a9b6a1.js"
+  }
+]);
